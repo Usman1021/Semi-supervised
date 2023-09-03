079d77b4a35cc1347899dbe0b7f059c1a3248620
Usman Muhammad <67257724+Usman1021@users.noreply.github.com>
2023-09-03 20:44:58 +0300

    Add files via upload

---
+++ readAndPreprocessImage.m
@@ -0,0 +1,14 @@
+ %%
+ % Resize images 
+     function Iout = readAndPreprocessImage(filename)
+
+       I = imread(filename);
+
+         if ismatrix(I)
+            I = cat(3,I,I,I);
+         end
+     
+
+           Iout = imresize(I, [224 224]);
+            
+     end
