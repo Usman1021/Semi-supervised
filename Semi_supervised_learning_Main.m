15a43d1c0727a8e347267d8a073cb9ff23d27ddb
Usman Muhammad <67257724+Usman1021@users.noreply.github.com>
2023-09-06 15:58:41 +0300

    Update Semi_supervised_learning_Main.m

--- Semi_supervised_learning_Main.m
+++ Semi_supervised_learning_Main.m
@@ -1,37 +1,31 @@
 
-% Define the path to your dataset folder (contains subfolders for each class)
-%% Input for Training set 
-rootFolder = fullfile('D:\four datasets\OCM to I\trainset');
-categories  = {'real','attack'};
+% Load the pre-trained DenseNet-201 model Using MATLAB 2023
+net = densenet201;
+%%
+% Create imageDatastore for labeled and unlabeled data
+rootFolder = fullfile('D:\ICASSP\four datasets\ICM to O\trainset');
+categories  = {'real1','attack1'};
 imdsLabeled = imageDatastore(fullfile(rootFolder, categories), 'IncludeSubfolders',true, ...
  'LabelSource','foldernames');
-trainingLabels = imdsLabeled.Labels;
 tbl = countEachLabel(imdsLabeled);
-
- %% Input for development set 
-rootFolder = fullfile('D:\four datasets\OCM to I\testset');
-categories  = {'real','attack'};
-DevelopmentSet  = imageDatastore(fullfile(rootFolder, categories), 'IncludeSubfolders',true, ...
- 'LabelSource','foldernames');
- 
-%% Input for UNLABELED DATA
-rootFolder = fullfile('D:\four datasets\OCM to I\unlabled data');
+%%
+% Create imageDatastore unlabeled data
+rootFolder = fullfile('D:\ICASSP\four datasets\ICM to O\unlabled data');
 categories  = {'50','80'};
 imdsUnlabeled  = imageDatastore(fullfile(rootFolder, categories), 'IncludeSubfolders',true, ...
  'LabelSource','foldernames');
 %%
-% Evaluate the final model on a test set (you should have a separate test dataset)
-rootFolder = fullfile('D:\four datasets\Replay Attack dataset\test\test');
-categories  = {'real','attack'};
-TestingSet  = imageDatastore(fullfile(rootFolder, categories), 'IncludeSubfolders',true, ...
+% Create imageDatastore validation data
+rootFolder = fullfile('D:\ICASSP\four datasets\ICM to O\testseet');
+categories  = {'real1','attack1'};
+imdsValidation  = imageDatastore(fullfile(rootFolder, categories), 'IncludeSubfolders',true, ...
  'LabelSource','foldernames');
- TestLables = TestingSet.Labels;
- 
-%% Model Input
-net = densenet201;
+
+%%
+% Densenet Model Initialization
 net.Layers(1)
 inputSize = net.Layers(1).InputSize;
-% Data augmentation
+
  imageAugmenter = imageDataAugmenter( ...
     'RandRotation',[-20,20], ...
     'RandXTranslation',[-3 3], ...
@@ -60,26 +54,41 @@ newClassLayer = classificationLayer('Name','new_classoutput');
 lgraph = replaceLayer(lgraph,classLayer.Name,newClassLayer);
 layers = lgraph.Layers;
 connections = lgraph.Connections;
-% Weights freezing
 layers(1:10) = freezeWeights(layers(1:10));
 lgraph = createLgraphUsingConnections(layers,connections);
-% Image resizing
-DevelopmentSet.ReadFcn = @(filename)readAndPreprocessImage(filename);
+
+%%
+% Set training options and Semi-supervised learning
+miniBatchSize = 32;
+numEpochs = 5;
+initialLearningRate = 0.001;
+lambda = 0.5; % Adjust the weight for labeled data
+imdsValidation.ReadFcn = @(filename)readAndPreprocessImage(filename);
+imdsUnlabeled.ReadFcn = @(filename)readAndPreprocessImage(filename);
 options = trainingOptions('adam', ...
-     'ExecutionEnvironment','gpu', ... 
-      'MiniBatchSize',32, ...
-       'InitialLearnRate',0.0001, ...
-    'MaxEpochs',5, ...
-     'ValidationData',{augimdsTrain1,DevelopmentSet}, ...
-     'ValidationFrequency',30, ...
-     'SequenceLength','longest', ...
-       'Verbose', false);
-% Model learning
-CNN1 = trainNetwork(augimdsTrain1,lgraph,options); 
-validationlabels = DevelopmentSet.Labels;
-% Image resizing 
-DevelopmentSet.ReadFcn = @(filename)readAndPreprocessImage(filename);
-[~, devlpscores1] = classify(CNN1,DevelopmentSet);
+    'MiniBatchSize', miniBatchSize, ...
+      'ExecutionEnvironment','gpu', ... 
+    'MaxEpochs', numEpochs, ...
+    'InitialLearnRate', initialLearningRate, ...
+    'ValidationData', imdsValidation, ...
+    'ValidationFrequency', 30, ...
+    'Verbose', true, ...
+    'Plots', 'training-progress', ...
+    'LearnRateSchedule', 'piecewise', ...
+    'LearnRateDropFactor', 0.1, ...
+    'LearnRateDropPeriod', 5, ...
+    'ExecutionEnvironment', 'auto', ...
+    'GradientThresholdMethod', 'absolute', ...
+    'GradientThreshold', 1, ... % Adjust the gradient threshold as needed
+    'OutputFcn', @(info)customOutputFunction(info, net, imdsUnlabeled, lambda));
+
+% Fine-tune the model with mixed labeled and pseudo-labeled data
+net = trainNetwork(augimdsTrain1, lgraph, options);
+%%
+% Evaluate the model on a validation set to compute EER
+validationlabels = imdsValidation.Labels;
+imdsValidation.ReadFcn = @(filename)readAndPreprocessImage(filename);
+[~, devlpscores1] = classify(net,imdsValidation);
 % Converting labels into numerical form
  numericLabels1 = grp2idx(validationlabels);
  numericLabels1(numericLabels1==2)= -1;
@@ -87,12 +96,16 @@ DevelopmentSet.ReadFcn = @(filename)readAndPreprocessImage(filename);
  [~,~,Info]=vl_roc(numericLabels1,devlpscores1(:,1));
  EER = Info.eer*100
  threashold1 = Info.eerThreshold;
- %%
- % Image resizing
-TestingSet.ReadFcn = @(filename)readAndPreprocessImage(filename);
+% Evaluate the final model on a test set (you should have a separate test dataset)
+rootFolder = fullfile('D:\ICASSP\four datasets\Oulu testing set\testset');
+categories  = {'real1','attack1'};
+imdsTest  = imageDatastore(fullfile(rootFolder, categories), 'IncludeSubfolders',true, ...
+ 'LabelSource','foldernames');
+ imdsTestlabels = imdsTest.Labels;
+imdsTest.ReadFcn = @(filename)readAndPreprocessImage(filename);
  % Evaluation for testing set for HTER 
- [~,  testscores1] = classify(CNN1,TestingSet);
- numericLabels = grp2idx(TestLables);
+ [~,  testscores1] = classify(net,imdsTest);
+ numericLabels = grp2idx(imdsTestlabels);
  numericLabels(numericLabels==2)= -1;
  numericLabels(numericLabels==1)= 1;
  real_scores1 =  testscores1(numericLabels==1);
@@ -100,95 +113,36 @@ TestingSet.ReadFcn = @(filename)readAndPreprocessImage(filename);
  FAR = sum(attack_scores2>threashold1) / numel(attack_scores2)*100;
  FRR = sum(real_scores1<=threashold1) / numel(real_scores1)*100;
  HTER1 = (FAR+FRR)/2
-[x1,y1,~,AUC1] = perfcurve(numericLabels, testscores1(:,1),1);
-AUC1
-plot(x1,y1,'-g','LineWidth',1.8,'MarkerSize',1.8)
-grid on
-hold on
 %%
-% Sem-Supervised learning stage
-% Self-training loop (repeat this for multiple iterations)
-numIterations = 3;
-confidenceThreshold = 0.9; % Adjust the confidence threshold as needed
-
-for iter = 1:numIterations
-imdsUnlabeled.ReadFcn = @(filename)readAndPreprocessImage(filename);
-    % Use the current model to predict labels for unlabeled data
-    predictedLabels = classify(CNN1, imdsUnlabeled);
-    predictedLabelsNumeric = double(predictedLabels);
-    confidenceScores = max(softmax(predictedLabelsNumeric), [], 2);
-    confidentIndices = confidenceScores > confidenceThreshold;
-    confidentSamples = imdsUnlabeled.Files(confidentIndices);
-    confidentLabels = predictedLabels(confidentIndices);
-    % Add confident samples to the labeled data
-    imdsLabeled.Files = [imdsLabeled.Files; confidentSamples];
-    imdsLabeled.Labels = [trainingLabels ; confidentLabels];
-    % Remove confident samples from the unlabeled data
-    imdsUnlabeled.Files(confidentIndices) = [];
- % augimdsTrain1.ReadFcn = @(filename)readAndPreprocessImage(filename);
-    % Train the model with the updated labeled data
-  CNN2 = trainNetwork(augimdsTrain1, lgraph, options);
-end
-
-validationlabels = DevelopmentSet.Labels;
-DevelopmentSet.ReadFcn = @(filename)readAndPreprocessImage(filename);
-[~, devlpscores2] = classify(CNN2,DevelopmentSet);
-% Converting labels into numerical form
- numericLabels1 = grp2idx(validationlabels);
- numericLabels1(numericLabels1==2)= -1;
- numericLabels1(numericLabels1==1)= 1;
- [~,~,Info]=vl_roc(numericLabels1,devlpscores2(:,1));
- EER = Info.eer*100
- threashold2 = Info.eerThreshold;
- %%
-% imdsTestlabels = imdsTest.Labels;
-TestingSet.ReadFcn = @(filename)readAndPreprocessImage(filename);
- % Evaluation for testing set for HTER 
- [~,  testscores2] = classify(CNN2,TestingSet);
- numericLabels = grp2idx(TestLables);
- numericLabels(numericLabels==2)= -1;
- numericLabels(numericLabels==1)= 1;
- real_scores1 =  testscores2(numericLabels==1);
- attack_scores2 =  testscores2(numericLabels==-1);
- FAR = sum(attack_scores2>threashold2) / numel(attack_scores2)*100;
- FRR = sum(real_scores1<=threashold2) / numel(real_scores1)*100;
- HTER2 = (FAR+FRR)/2
-[x2,y2,~,AUC2] = perfcurve(numericLabels, testscores2(:,1),1);
-AUC2
- plot(x2,y2,'-m','LineWidth',1.8,'MarkerSize',1.8)
- grid on
- hold on
-%% Features extraction based on the last average pooling layer of FINE-TUNED model for the training set 
+%Semi-supervised with RNN
+% Features extraction based on the last average pooling layer of ResNet for the training set 
 featureLayer = 'avg_pool' ;
 imdsLabeled.ReadFcn = @(filename)readAndPreprocessImage(filename);
-trainingFeatures1 = activations(CNN2, imdsLabeled, featureLayer, ...
+trainingFeatures1 = activations(net, imdsLabeled, featureLayer, ...
     'MiniBatchSize', 32, 'OutputAs', 'columns');
-%% Features extraction based on the last average pooling layer of  FINE-TUNED model  for the development set 
-DevelopmentSet.ReadFcn = @(filename)readAndPreprocessImage(filename);
-developmentFeatures1 = activations(CNN2, DevelopmentSet, featureLayer, ...
+%% Features extraction based on the last average pooling layer of ResNet for the development set 
+imdsValidation.ReadFcn = @(filename)readAndPreprocessImage(filename);
+developmentFeatures1 = activations(net, imdsValidation, featureLayer, ...
     'MiniBatchSize', 32, 'OutputAs', 'columns');
 
-%% Features extraction based on the last average pooling layer of FINE-TUNED model for the testing set 
+%% Features extraction based on the last average pooling layer of ResNet for the testing set 
 % Extract testing set features using the CNN
-TestingSet.ReadFcn = @(filename)readAndPreprocessImage(filename);
-testingFeatures1 = activations(CNN2, TestingSet, featureLayer, ...
+imdsTest.ReadFcn = @(filename)readAndPreprocessImage(filename);
+testingFeatures1 = activations(net, imdsTest, featureLayer, ...
     'MiniBatchSize', 32, 'OutputAs', 'columns');
 %%
-
 rng(13);
 trainf = {};
 trainf{end+1} =  trainingFeatures1;
 
 trainlabl = {};
-trainlabl{end+1} = trainingLabels';
+trainlabl{end+1} = trainingLabels1';
 
 train1 = {};
 train1{end+1} = developmentFeatures1;
 % 
 train2 = {};
 train2{end+1} = validationlabels';
-
-
 numFeatures = 1920;
  numHiddenUnits =100;
 numClasses = 2;
@@ -205,7 +159,8 @@ options = trainingOptions('adam', ...
     'ValidationData',{train1,train2}, ...
     'ValidationFrequency',30, ...
     'GradientThreshold',1, ...
-       'Verbose', false);
+      'Plots','training-progress',...
+   'OutputFcn',@(info)stopIfAccuracyNotImproving(info,3));
 
 RNN = trainNetwork(trainf',trainlabl,layers,options);
 
@@ -224,18 +179,11 @@ RNN = trainNetwork(trainf',trainlabl,layers,options);
  % Evaluation for testing set for HTER 
  [~,  testscores3] = classify(RNN, testingFeatures1);
  testscores3 =testscores3';
- numericLabels = grp2idx(TestLables);
+ numericLabels = grp2idx(imdsTestlabels);
  numericLabels(numericLabels==2)= -1;
  numericLabels(numericLabels==1)= 1;
  real_scores1 =  testscores3(numericLabels==1);
  attack_scores2 =  testscores3(numericLabels==-1);
  FAR = sum(attack_scores2>threashold3) / numel(attack_scores2)*100;
  FRR = sum(real_scores1<=threashold3) / numel(real_scores1)*100;
- HTER3 = (FAR+FRR)/2
-[x3,y3,~,AUC3] = perfcurve(numericLabels, testscores3 (:,1),1);
-AUC3
-plot(x3,y3,'-c','LineWidth',1.8,'MarkerSize',1.8)
-grid on
-hold off
-legend('Supervised learning', 'Semi-Supervised learning','Semi-Supervised + RNN')
-
+ HTER2 = (FAR+FRR)/2
